f58635481c974dca079ac57ce2ba14edde9c1b6b
jreivilo <j.reivilo@outlook.com>
2024-06-08 15:50:12 +0200

    chore: Update Dockerfile and package.json for backend server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const fastify = require('fastify')({ logger: true })
 
 fastify.get('/', async (request, reply) => {  return { hello: 'world' }});
-fastify.listen(3000, (err, address) => {  
+fastify.listen(3000, '0.0.0.0', (err, address) => {  
     if (err) throw err;  
     fastify.log.info(`server listening on ${address}`);
 })
