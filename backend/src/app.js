d36691afb821a77135e57058b21394a26b63fd8d
shaolin-peanut <shaolinpeanut@gmail.com>
2024-07-10 18:03:02 +0200

    starting fill info

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,6 @@
 const path = require('node:path')
 const AutoLoad = require('@fastify/autoload')
 
-// Pass --options via CLI arguments in command to enable these options.
 const options = {}
 
 module.exports = async function (fastify, opts) {
@@ -12,25 +11,16 @@ module.exports = async function (fastify, opts) {
     routePrefix: '/docs',
     })
   fastify.register(require('@fastify/formbody'));
-  //fastify.register(require('@fastify/json'));
 
-  // Do not touch the following lines
-
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
     options: Object.assign({}, opts)
   })
 
-  // This loads all plugins defined in routes
-  // define your routes in one of these
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'routes'),
     options: Object.assign({}, opts)
   })
-
 
   // cors plugin
   fastify.register(require('@fastify/cors'), {
